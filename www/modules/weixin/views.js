96668040d295cdd8f1d89a80abbacb26352a284d
bqw_5189 <bqw_5189@163.com>
2015-03-16 11:47:59 +0800

    weixin list

--- www/modules/weixin/views.js
+++ www/modules/weixin/views.js
@@ -5,6 +5,7 @@ weixin.config(function($stateProvider, $urlRouterProvider) {
     views: {
       'home-weixin-view': {
         templateUrl: "modules/weixin/weixin.html",
+        controller:'WeiXinCtrl'
       }
     }
   });
