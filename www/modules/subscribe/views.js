bf0a203b977e2c041b9a67b0afcb696d260ddb15
bqw_5189 <bqw_5189@163.com>
2015-03-23 11:55:11 +0800

    subscribe

--- www/modules/subscribe/views.js
+++ www/modules/subscribe/views.js
@@ -2,13 +2,18 @@ weixin.config(function($stateProvider, $urlRouterProvider) {
   $stateProvider
     .state('subscribe', {
     url: "/subscribe",
-    templateUrl: "modules/subscribe/page.html",
+    templateUrl: "modules/subscribe/page.html"
+  })
+  .state('subscribe.list', {
+    url: "/list",
+    templateUrl: "modules/subscribe/list.html",
     controller:'SubscribeCtrl'
   })
   .state('subscribe.detail', {
-    url: "/subscribe/detail",
-    templateUrl: "modules/subscribe/detail.html",
-    controller:'SubscribeCtrl'
-  })
+	    url: "/detail",
+	    templateUrl: "modules/subscribe/detail.html",
+	    controller:'SubscribeCtrl'
+	  })
   ;
+  $urlRouterProvider.when('/subscribe','/subscribe/list');
 });
